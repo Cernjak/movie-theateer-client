f7b02eae040b0c59c6952ab10951d8a96ed791c1
Igor Cernjak <cernjakigor@gmail.com>
2024-09-16 00:17:17 +0200

    improve accessibility

--- app/components/MovieList/MovieList.tsx
+++ app/components/MovieList/MovieList.tsx
@@ -16,22 +16,20 @@ const MovieList = ({ movies }: IProps) => {
     <div className="movie-list">
       <ul className="movie-list__list">
         {movies?.map((movie) => (
-          <Link
-            className="movie-list__item"
-            href={`/movies/${movie.slug}`}
-            key={movie._id}
-          >
-            <span className="movie-list__img-container img-container">
-              <Image
-                className="movie-list__img cover-img"
-                src={`https://image.tmdb.org/t/p/w500/${movie.posterPath}`}
-                alt={movie.title}
-                width={400}
-                height={800}
-              />
-            </span>
-            <h2 className='movie-list__title'>{movie.title}</h2>
-          </Link>
+          <li className="movie-list__item" key={movie._id}>
+            <Link href={`/movies/${movie.slug}`}>
+              <span className="movie-list__img-container img-container">
+                <Image
+                  className="movie-list__img cover-img"
+                  src={`https://image.tmdb.org/t/p/w500/${movie.posterPath}`}
+                  alt="Movie poster"
+                  width={400}
+                  height={800}
+                />
+              </span>
+              <h2 className="movie-list__title">{movie.title}</h2>
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
