3e5ee76178837176c0c6a09be8d635148418b25a
Igor Cernjak <cernjakigor@gmail.com>
2024-09-15 23:51:15 +0200

    add domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.tmdb.org', '127.0.0.1']
-  }
+    domains: [
+      'image.tmdb.org',
+      '127.0.0.1',
+      'https://movie-theater-rust.vercel.app/',
+    ],
+  },
 };
 
 export default nextConfig;
